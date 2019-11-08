c1a08f1cd8fd0e933e7fa06a0f6e52af5c22ea09
Chris A <ca033@hdm-stuttgart.de>
2019-11-08 11:42:05 +0100

    Updated API to use fake database calls to test async data requests

--- app.js
+++ app.js
@@ -2,20 +2,36 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+// Set CORS-Headers to allow cross domain requests
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/api/projects', function (req, res) {
   const projects = [{
-      id: 'server project1'
+      id: 'abc123abc123'
     },
     {
-      id: 'server project2'
-    },
-    {
-      id: 'server project3'
-    },
-  ]
-  res.json(projects);
+      id: '789xyz789xyz'
+    }
+  ];
+
+  fakeDatabaseCall(projects).then(data => {
+    res.json(data);
+  })
+
 });
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
+
+function fakeDatabaseCall(data) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(data);
+    }, 1000);
+  })
+}
