87ede0ab36d70f082976761170bc34a14628315c
Chris A <ca033@hdm-stuttgart.de>
2019-11-20 16:33:07 +0100

    Cleanup and comments

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,3 +1,4 @@
+// The Vue CLI uses Babel to compile the newest ECMA-Script to a more broadly supported version
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,3 +1,5 @@
+// We are using Jest as our frontend testing framework
+// We need to tell Jest to use the appropriate preset for the Vue CLI
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest'
 }
